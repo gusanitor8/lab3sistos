de24b272e7ca064467fcf1190d803a25b19e398a
gusanitor8 <gusanitor8@gmail.com>
2024-02-21 13:14:04 -0600

    first fork made

--- SudokuValidator.c
+++ SudokuValidator.c
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <pthread.h>
 
 #define ROWS 9
 #define COLUMNS 9
@@ -71,11 +74,67 @@ int print_sudoku(){
         for(int j = 0; j < COLUMNS; j++) {
             printf("%d ", sudoku[i][j]);
         }        
-        printf("\n");
+        printf("\n"); 
     }
+    printf("\n"); 
+
     return 0;
 }
 
+bool check_three_by_three(int index){
+    int validation[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; 
+
+    for (int row = 0; row < 3; row++){
+        for (int col = 0; col < 3; col++){
+            int number = sudoku[row + (index / 3) * 3][col + (index % 3) * 3];
+            
+            if (validation[number - 1] == 0){
+                validation[number - 1] = 1;
+            }else{
+                return false;
+            }            
+        }
+    }
+
+    return true;
+}
+
+
+/**
+ * @brief Validates the sudoku in either a row or a column
+ * 
+ * @param index the index of the row or column
+ * @param axis the orientation of the validation (0 for row, 1 for column)
+ * @return true the sudoku is valid
+ * @return false the sudoku is invalid
+ */
+bool check_line(int index, int axis){
+    int validation[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};     
+
+    if (axis == 0){
+        for (int columns = 0; columns < COLUMNS; columns++){
+            int number = sudoku[index][columns];
+            
+            if (validation[number - 1] == 0){
+                validation[number - 1] = 1;
+            }else{
+                return false;
+            }
+        }
+    }else{
+        for (int rows = 0; rows < ROWS; rows++){
+            int number = sudoku[rows][index];
+            
+            if (validation[number - 1] == 0){
+                validation[number - 1] = 1;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 
 /**
  * @brief main function
@@ -91,10 +150,25 @@ int main(int argc, char *argv[]){
     void* mapped_data = file_data.mapped_data;
     off_t file_size = file_data.file_size;
     
-    copy_sudoku(mapped_data, file_size);
-
-    // Print the sudoku
+    copy_sudoku(mapped_data, file_size);    
     print_sudoku();
+
+    pid_t pid = getpid();
+    // printf("PID: %d\n", pid);
     
+    pid_t child = fork();
+
+    if (child == 0){
+        // child process        
+        char pid_str[20];
+        sprintf(pid_str, "%d", pid);
+
+        execlp("ps", "ps", "-p", pid_str, "-lLf", NULL);
+    }else{
+        // parent process
+
+        wait(NULL);
+    }
+
     return 0;
 }
